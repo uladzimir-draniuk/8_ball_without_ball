bfe9e6447ad7ab061a3955a65a84084a371d41af
uladzimir-draniuk <uladzimir.draniuk@gmail.com>
2021-10-19 13:42:07 +0300

    Initial Commit

---
+++ 8_ball_without_ball/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  8_ball_without_ball
+//
+//  Created by elf on 19.10.2021.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
